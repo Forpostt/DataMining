62f154ec6cb9ca3b145bb5b6938a4869fc77a07e
pmankevich <pmankevich@yandex-team.ru>
2019-04-18 21:27:42 +0300

    update ials

--- ImplicitALS/models.py
+++ ImplicitALS/models.py
@@ -3,6 +3,7 @@
 import numpy as np
 import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.utils.extmath import randomized_svd
 from sklearn.linear_model import Ridge
 
 from utils import *
@@ -93,28 +94,40 @@ def linear(train):
     return results
 
 
-def ials(train, test, init, n_iter=10, n_features=5, lambda_val=8, alpha=25):
+def ials(train, test, init, n_iter=10, n_features=6, lambda_val=8, alpha=75):
     user_size, item_size = train.shape
 
-    users = np.random.normal(size=(user_size, n_features))
-    items = np.random.normal(size=(item_size, n_features))
+    U, S, V = randomized_svd(init, n_components=n_features, n_iter=n_iter, random_state=1234)
 
-    lambda_I = lambda_val * np.eye(n_features)
-    confidence = np.ones(train.shape) + train * alpha
+    users = np.hstack([np.ones((user_size, 1)), U.copy()])
+    items = np.hstack([np.ones((item_size, 1)), V.T.copy()])
+
+    lambda_I = lambda_val * np.eye(n_features + 1)
+    # confidence = np.ones(train.shape) + train * alpha
+    confidence = np.ones(train.shape) + alpha * np.log(train + 1)
+
+    user_bias = np.zeros(shape=(user_size,))
+    item_bias = np.zeros(shape=(item_size,))
 
     for it in range(n_iter):
         for u in range(user_size):
             A = np.dot(items.T * confidence[u], items) + lambda_I * (train[u] > 0).sum()
-            b = np.dot(items.T * confidence[u], init[u])
+            b = np.dot(items.T * confidence[u], init[u] - item_bias)
             users[u] = np.linalg.solve(A, b)
+        user_bias = users[:, 0].copy()
+        users[:, 0] = 1
 
         for i in range(item_size):
             A = np.dot(users.T * confidence[:, i], users) + lambda_I * (train[:, i] > 0).sum()
-            b = np.dot(users.T * confidence[:, i], init[:, i])
+            b = np.dot(users.T * confidence[:, i], init[:, i] - user_bias)
             items[i] = np.linalg.solve(A, b)
+        item_bias = items[:, 0].copy()
+        items[:, 0] = 1
 
-        res = np.dot(users, items.T)
-        rmse = np.sqrt(((res * (test > 0) - test) ** 2).sum() / (test > 0).sum())
+        res = np.dot(users[:, 1:], items[:, 1:].T) + user_bias.reshape(-1, 1) + item_bias
+        rmse = (res - test) ** 2
+        rmse[test == 0] = 0
+        rmse = (rmse.sum() / (test > 0).sum())**0.5
         print(rmse)
 
     return res
